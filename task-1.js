d28d54d96ee07703bcb65b67e08fb07f703bb8bd
VeronikaVaraksina1 <plaguemoon@gmail.com>
2023-10-13 21:03:44 +0300

    added js files

---
+++ task-1.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function makeTransaction(quantity, pricePerDroid) {
+  return console.log(
+    `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`
+  );
+}
+
+// console.log(makeTransaction(5, 3000));
+// console.log(makeTransaction(3, 1000));
+// console.log(makeTransaction(10, 500));
+
+makeTransaction(5, 3000);
+makeTransaction(3, 1000);
+makeTransaction(10, 500);
---
+++ task-3.js
@@ -0,0 +1,14 @@
+"use strict";
+
+// function getElementWidth(content, padding, border) {
+//   return content + padding + border;
+// }
+
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+// console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+let getElementWidth = "24.6px";
+console.log("Element width: ", getElementWidth);
+
+console.log(Number.parseInt(getElementWidth));
