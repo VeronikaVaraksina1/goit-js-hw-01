cf1c0e26beae5b7e54631dc4cff9e0981d094500
VeronikaVaraksina1 <plaguemoon@gmail.com>
2023-10-15 13:45:00 +0300

    finished tasks

--- task-3.js
+++ task-3.js
@@ -1,14 +1,13 @@
 "use strict";
 
-// function getElementWidth(content, padding, border) {
-//   return content + padding + border;
-// }
+function getElementWidth(content, padding, border) {
+  const contentWidth = Number.parseFloat(content);
+  const paddingWidth = Number.parseFloat(padding);
+  const borderWidth = Number.parseFloat(border);
 
-// console.log(getElementWidth("50px", "8px", "4px")); // 74
-// console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-// console.log(getElementWidth("200px", "0px", "0px")); // 200
+  return contentWidth + (paddingWidth + borderWidth) * 2;
+}
 
-let getElementWidth = "24.6px";
-console.log("Element width: ", getElementWidth);
-
-console.log(Number.parseInt(getElementWidth));
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
